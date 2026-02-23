253c07b8210fb5cd2dbfd2632ba9d44c578db3a3
Arjun <53638468+Star-codi@users.noreply.github.com>
2026-02-23 13:34:58 +0530

    Fix duplicate print statement in Sample.java

--- Sample.java
+++ Sample.java
@@ -1,6 +1,6 @@
 class Sample{
  public static void main(String[] args){
    System.out.println("Hello from DevOps");
-   System.out.println("2- Hello from DevOps");
+   System.out.println("Hello from DevOps");
  }
 }
