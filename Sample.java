d35ee9c8e1d43462a5d6d160803efab36cbb1fc5
Arjun <53638468+Star-codi@users.noreply.github.com>
2026-02-23 13:37:54 +0530

    Change greeting message from 'DevOps' to 'jenkins'

--- Sample.java
+++ Sample.java
@@ -1,6 +1,6 @@
 class Sample{
  public static void main(String[] args){
    System.out.println("Hello from DevOps");
-   System.out.println("Hello from DevOps");
+   System.out.println("Hello from jenkins");
  }
 }
