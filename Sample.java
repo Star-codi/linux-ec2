5c5e904646ceb74057dedb5a85fe9b2211e2e334
Arjun <53638468+Star-codi@users.noreply.github.com>
2026-02-23 13:40:03 +0530

    Update print statements in Sample.java

--- Sample.java
+++ Sample.java
@@ -1,6 +1,8 @@
 class Sample{
  public static void main(String[] args){
+  for(int i=1;i<=10;i++){
    System.out.println("Hello from DevOps");
-   System.out.println("Hello from jenkins");
+   System.out.println("Hello from jenkins
+    }
  }
 }
